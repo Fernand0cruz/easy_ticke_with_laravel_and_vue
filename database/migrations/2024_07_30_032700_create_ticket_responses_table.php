7c80a6e40b94986908e793fe2959502d232feb17
Fernando <cruzfernando.dev@gmail.com>
2024-08-09 14:47:52 -0300

    Update ticket_responses_table migration to use longText for response column

--- database/migrations/2024_07_30_032700_create_ticket_responses_table.php
+++ database/migrations/2024_07_30_032700_create_ticket_responses_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id(); // Cria uma coluna 'id' do tipo BIGINT com auto-incremento e chave primária
             $table->foreignId('ticket_id')->constrained()->onDelete('cascade');// Cria uma coluna 'ticket_id' como chave estrangeira referenciando 'id' na tabela 'tickets' com exclusão em cascata
             $table->foreignId('user_id')->constrained()->onDelete('cascade');// Cria uma coluna 'user_id' como chave estrangeira referenciando 'id' na tabela 'users' com exclusão em cascata (usuário que respondeu)
-            $table->text('response'); // Cria uma coluna 'response' do tipo TEXT para armazenar o texto da resposta
+            $table->longText('response'); // Cria uma coluna 'response' do tipo TEXT para armazenar o texto da resposta
             $table->timestamps(); // Cria as colunas 'created_at' e 'updated_at' para controlar as datas de criação e atualização
         });
     }
